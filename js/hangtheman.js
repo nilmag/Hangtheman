83a264b1497a806ff9e1e1bde7fb0262f6e74356
Magnus Nilsen <magnusni@gmail.com>
2013-08-28 23:54:41 +0200

    added some new tests

--- js/hangtheman.js
+++ js/hangtheman.js
@@ -1,5 +1,15 @@
 var words = ['Javascript', 'Programming', 'TestDriven-Development', 'Hanttheman'];
 
 var hangtheman =  {
-	var 
+	maskTheWord: function(word, chars) {
+		console.log(chars.join(''));
+		var regex = new RegExp('[^' + chars.join() + ']', 'gi');
+
+		word = word.replace(regex, '*');
+
+		return word;
+	},
+	guessedSolution: function(solution, guesses) {
+		return this.maskTheWord(solution, guesses) === solution;
+	}
 };
--- js/tests.js
+++ js/tests.js
@@ -3,7 +3,19 @@ $(document).ready(function() {
         equal(true, true, 'passed');
     });
 
-    test('Get random word', function() {
-        
+    test('mask the word', function() {
+        equal(hangtheman.maskTheWord('javascript', ['o']), '**********', 'Masked the word with no hits');
+        equal(hangtheman.maskTheWord('javascript', ['j', 'v', 'a', ',']), 'java******', 'Masked the word with some hits');
+        equal(hangtheman.maskTheWord('JaVa', ['j', 'v']), 'J*V*', 'Masked the word ignoring case');
+        equal(hangtheman.maskTheWord('javascript2', ['a', '2']), '*a*a******2', 'Masked the word with some hits and numeric');
+        equal(hangtheman.maskTheWord('jav ascript2', [' ', '2']), '*** *******2', 'Masked the word with some hits, numeric and space');
+        equal(hangtheman.maskTheWord('ja-va', ['j', '-']), 'j*-**', 'Masked the word with dash');
+    });
+
+    test('Guessed the solution', function() {
+        equal(hangtheman.guessedSolution('java', ['j', 'a', 'v']), true, 'Guessed the solution with similar case');
+        equal(hangtheman.guessedSolution('Java', ['j', 'a', 'v']), true, 'Guessed the solution without similar case');
+        equal(hangtheman.guessedSolution('java', ['J', 'a', 'v']), true, 'Guessed the solution without similar case');
+        equal(hangtheman.guessedSolution('java', ['j', 'v']), false, 'Did not guess the solution');
     });
 });
